0b0ad62eb5fbb1869b682bd87ec78312f19203fc
Ari <82783339+Arihany@users.noreply.github.com>
2026-04-18 04:26:12 +0900

    Comment out WCP_VERSION_SUFFIX in dxvk-sarek-dyasync.sh

--- scripts/profiles/dxvk-sarek-dyasync.sh
+++ scripts/profiles/dxvk-sarek-dyasync.sh
@@ -10,6 +10,6 @@ WCP_TYPE="DXVK"
 WCP_VERSION_CODE=0
 #WCP_VERSION_CODE_DEFAULT=0
 #WCP_VERSION_PREFIX="async-"
-WCP_VERSION_SUFFIX="-dyasync"
+#WCP_VERSION_SUFFIX="-dyasync"
 
 WCP_DESC="Built on the Winlator WCP Hub (Upstream: pythonlover02)"
